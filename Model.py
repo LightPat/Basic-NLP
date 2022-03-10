9bacf2c6ec7cb7434e4e7f5632713370c9e7e689
LightPat <60397186+LightPat@users.noreply.github.com>
2022-03-10 11:34:50 -0500

    Tokenizer for training and testing data

--- Model.py
+++ Model.py
@@ -6,65 +6,136 @@ Created on Tue Mar  8 11:58:37 2022
 """
 
 import os
+import re
 import csv
 import time
 import random
+import string
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+
+# def custom_standardization(input_data):
+#   lowercase = tf.strings.lower(input_data)
+#   stripped_html = tf.strings.regex_replace(lowercase, '<br />', ' ')
+#   return tf.strings.regex_replace(stripped_html,
+#                                   '[%s]' % re.escape(string.punctuation),
+#                                   '')
+
+max_features = 10000
+sequence_length = 250
+
+# vectorize_layer = layers.TextVectorization(standardize=custom_standardization,
+#     max_tokens=max_features,
+#     output_mode='int',
+#     output_sequence_length=sequence_length)
+
+
+train_list = []
+test_list = []
+with open('Train_Set.csv', encoding="utf8") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    train_list = list(csv_reader)
+    
+with open('Test_Set.csv', encoding="utf8") as csv_file:
+    csv_reader = csv.reader(csv_file)
+    test_list = list(csv_reader)
+
+random.shuffle(train_list)
+random.shuffle(test_list)
+
+# Separate data and labels
+train_data = []
+train_labels = []
+for row in train_list:
+    if row[2] == "Irrelevant":
+        continue
+    train_data.append(row[3])
+    train_labels.append(row[2])
+
+test_data = []
+test_labels = []
+for row in test_list:
+    if row[2] == "Irrelevant":
+        continue
+    test_data.append(row[3])
+    test_labels.append(row[2])
+    
+    
+# train_data = np.array(train_data)
+# test_data = np.array(test_data)
+
+# # One hot encode the labels
+# outputCategories = ["Positive", "Neutral", "Negative"]
+
+# train_indexes = []
+# for label in train_labels:
+#     train_indexes.append(outputCategories.index(label))
+
+# train_labels = np.array(train_indexes)
+# train_labels = keras.utils.to_categorical(train_labels, len(outputCategories))
+
+
+# test_indexes = []
+# for label in test_labels:
+#     test_indexes.append(outputCategories.index(label))
+
+# test_labels = np.array(test_indexes)
+# test_labels = keras.utils.to_categorical(test_labels, len(outputCategories))
+
+
+# Hyperparameteres
+vocab_size = 20000
+oov_token = "<OOV>"
+padding_type = "post"
+truncating_type = "post"
+embedding_dim = 16
+max_length = 25
+
+# Tokenize the training data
+tokenizer = Tokenizer(num_words=vocab_size, oov_token=oov_token)
+tokenizer.fit_on_texts(train_data)
+word_index = tokenizer.word_index
+
+# Pad the sequences so that they are all the same length
+train_sequences = tokenizer.texts_to_sequences(train_data)
+train_padded = pad_sequences(train_sequences, padding=padding_type, truncating=truncating_type, maxlen=max_length)
+
+test_sequences = tokenizer.texts_to_sequences(test_data)
+test_padded = pad_sequences(test_sequences, padding=padding_type, truncating=truncating_type, maxlen=max_length)
+
+# Label generation
+label_tokenizer = Tokenizer()
+label_tokenizer.fit_on_texts(train_labels)
+
+train_label_seq = np.array(label_tokenizer.texts_to_sequences(train_labels))
+test_label_seq = np.array(label_tokenizer.texts_to_sequences(test_labels))
+
+print(train_label_seq[0])
+
+# with tf.device('/device:GPU:0'):
+model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.Flatten(),
+    # tf.keras.layers.GlobalAveragePooling1D(),
+    tf.keras.layers.Dense(6, activation='relu'),
+    tf.keras.layers.Dense(3, activation='sigmoid')
+])
+
+model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+print(model.summary())
+
+history = model.fit(train_padded, train_label_seq, epochs=30,
+                    validation_data=(test_padded, test_label_seq), verbose=1)
+
+
+# test_seq = tokenizer.texts_to_sequences(test_data[0:2])
+# padded = pad_sequences(test_seq, padding="post", truncating="post")
 
 """
-serializeDataSet = False
-
-if serializeDataSet == True:
-    # Import input data
-    print("Importing data from csv")
-    input_data = []
-    with open('Twitter Dataset.csv', encoding="utf8") as csv_file:   
-        csv_reader = csv.reader(csv_file)
-        input_data = list(csv_reader)
-    
-    # Separate data into training and test sets
-    test_list = []
-    
-    count = 0
-    # 20% of my data should be in my test set
-    max_test_count = int(len(input_data) * 0.2)
-    
-    for row in input_data:
-        count += 1
-        index = random.randint(0, len(input_data)-1)
-        
-        test_list.append(input_data[index])
-        if (count == max_test_count):
-            break
-    
-    train_list = []
-    for row in input_data:
-        if not row in test_list:
-            train_list.append(row)
-
-    print(len(train_list), len(test_list), len(train_list) + len(test_list))
-    
-    # Save lists as csv files
-    with open('Test_Set.csv', 'w', encoding="utf8", newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerows(test_list)
-        
-    with open('Train_Set.csv', 'w', encoding="utf8", newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        csv_writer.writerows(train_list)
-        
-    print("Completed serialization of training and testing datasets.")
-"""
-
-
-
-
-
-
-
 # tf.debugging.set_log_device_placement(True)
 print("\n", tf.config.list_physical_devices(), "\n", tf.config.list_logical_devices(), "\n")
 
@@ -77,62 +148,20 @@ with tf.device('/device:CPU:0'):
 with tf.device('/device:GPU:0'):
     start = time.time()
     
-    train_list = []
-    test_list = []
-    with open('Train_Set.csv', encoding="utf8") as csv_file:
-        csv_reader = csv.reader(csv_file)
-        train_list = list(csv_reader)
-        
-    with open('Test_Set.csv', encoding="utf8") as csv_file:
-        csv_reader = csv.reader(csv_file)
-        test_list = list(csv_reader)
-    
-    random.shuffle(train_list)
-    random.shuffle(test_list)
-    
-    # Separate data and labels
-    train_data = []
-    train_labels = []
-    for row in train_list:
-        train_data.append([row[3]])
-        train_labels.append(row[2])
-    
-    test_data = []
-    test_labels = []
-    for row in test_list:
-        test_data.append([row[3]])
-        test_labels.append(row[2])
-    
-    train_data = np.array(train_data)
-    test_data = np.array(test_data)
-    
-    # One hot encode the labels
-    outputCategories = ["Positive", "Neutral", "Negative", "Irrelevant"]
-    
-    train_indexes = []
-    for label in train_labels:
-        train_indexes.append(outputCategories.index(label))
-    
-    train_labels = np.array(train_indexes)
-    train_labels = keras.utils.to_categorical(train_labels, len(outputCategories))
-    
-    
-    test_indexes = []
-    for label in test_labels:
-        test_indexes.append(outputCategories.index(label))
-    
-    test_labels = np.array(test_indexes)
-    test_labels = keras.utils.to_categorical(test_labels, len(outputCategories))
-    
     print(train_data.shape, train_labels.shape, test_data.shape, test_labels.shape)
     
-    # The model
-    model = keras.Sequential()    
-    # model.add(keras.Input(shape=(1,)))
-    model.add(layers.Embedding(1000, 64))
-    model.add(layers.Dense(4))
+    tokenizer = Tokenizer(num_words=20000)
+    tokenizer.fit_on_texts(train_data[0])
     
-    print(model.summary())
+    # use tf-idf labels later
+    
+    # The model
+    # model = keras.Sequential()    
+    # # model.add(keras.Input(shape=(1,)))
+    # model.add(layers.Embedding(1000, 64))
+    # model.add(layers.Dense(3))
+    
+    # print(model.summary())
     
     
     
@@ -144,11 +173,11 @@ with tf.device('/device:GPU:0'):
 
     # print(model.summary())
 
-    model.compile('rmsprop', 'mse')
+    # model.compile('rmsprop', 'mse')
 
-    input_array = train_data[0]
-    output_array = model.predict(input_array)
-    print(output_array)
+    # input_array = train_data[0]
+    # output_array = model.predict(input_array)
+    # print(output_array)
     
     # end = time.time()
-    # print("Time elapsed for GPU", end - start)
+    # print("Time elapsed for GPU", end - start)"""
